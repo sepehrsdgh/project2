e06f975982f8abf11944a372c3dbd859fd18174c
sepehr <sepehrdesdgh@gmail.com>
2022-08-07 23:50:23 +0430

    7 AUGUST 3

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
-let scroolOBJECT=document.querySelector(".hamburger-open")
+//show more and show less 1//
+number=document.querySelectorAll(".list2").length
+
+document.querySelector(".numberofshowmore").innerHTML=`Show ${number} More`
+
+function display(){
+    for(let i=0;i<document.querySelectorAll(".list1").length;i++){
+    document.querySelectorAll(".list1")[i].style.display="none"
+    }
+}
+display()
+
+document.querySelector("#showmore").addEventListener("click",showMore)
+document.querySelector("#showless").addEventListener("click",showLess)
+
+
+function showMore(){
+    for(let i=0;i<document.querySelectorAll(".list1").length;i++){
+    document.querySelectorAll(".list1")[i].style.display="flex"
+    }
+    document.querySelector("#showmore").style.display="none"
+}
+
+function showLess(){
+    for(let i=0;i<document.querySelectorAll(".list1").length;i++){
+    document.querySelectorAll(".list1")[i].style.display="none"
+    }
+    document.querySelector("#showmore").style.display="flex"
+}
+
+//show more and show less 2//
+function display2(){
+    for(let i=0;i<document.querySelectorAll(".list2").length;i++){
+    document.querySelectorAll(".list2")[i].style.display="none"
+    }
+    
+}
+display2();
+document.querySelector("#showmore2").addEventListener("click",showMore2)
+document.querySelector("#showless2").addEventListener("click",showLess2)
+function showMore2(){
+    for(let i=0;i<document.querySelectorAll(".list2").length;i++){
+    document.querySelectorAll(".list2")[i].style.display="flex"
+    }
+    document.querySelector("#showmore2").style.display="none"
+}
+
+function showLess2(){
+    for(let i=0;i<document.querySelectorAll(".list2").length;i++){
+    document.querySelectorAll(".list2")[i].style.display="none"
+    }
+    document.querySelector("#showmore2").style.display="flex"
+}
